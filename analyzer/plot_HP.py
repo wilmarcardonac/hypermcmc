8b8551ae7b2c0c026290cd65abaad3aa199e9306
Wilmar Alberto Cardona Castro <wilmar.cardona@unige.ch>
2015-10-25 13:43:23 +0100

    SCRIPT TO PLOT MODIFIED TO PLOT W BAND MAGNITUDE RESIDUALS AND CODIFY HPs WITH COLOR

--- analyzer/plot_HP.py
+++ analyzer/plot_HP.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as py
 
 host = ['n4536','n4639','n3982','n3370','n3021','n1309','n4038','n5584','n4258'] 
 
-marker = ['bo','gv','r^','k<','y>','mD','b+','gx','r*']
+marker = ['o','v','^','<','>','D','+','x','*']
 
-alpha_eff = np.dtype([('Period',np.float32),('alpha',np.float32),('name',np.str_,5)])
+alpha_eff = np.dtype([('Period',np.float32),('residual',np.float32),('error',np.float32),('alpha',np.float32),('name',np.str_,5)])
 
-P,HP,galaxy = np.loadtxt('../output/effective_hyperparameters_cepheids.txt',unpack=True,usecols=[0,1,2],dtype=alpha_eff)
+P,re,er,HP,galaxy = np.loadtxt('../output/effective_hyperparameters_cepheids.txt',unpack=True,usecols=[0,1,2,3,4],dtype=alpha_eff)
 
 indexs = 0
 
@@ -21,7 +21,27 @@ for index in range(len(P)):
 
         indexf = index
 
-py.plot(P[indexs:indexf+1],HP[indexs:indexf+1],marker[0],label=host[0])
+for index in range(indexs,indexf+1):
+
+    if HP[index] == 1.:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[0],mfc='k',fmt='',ls='None',label=host[0])
+
+    elif HP[index] < 1. and HP[index] >= .5:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[0],mfc='b',fmt='',ls='None',label=host[0])
+
+    elif HP[index] < .5 and HP[index] >= .3:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[0],mfc='g',fmt='',ls='None',label=host[0])
+
+    elif HP[index] < .3 and HP[index] >= .1:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[0],mfc='r',fmt='',ls='None',label=host[0])
+
+    elif HP[index] < .1 :
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[0],mfc='y',fmt='',ls='None',label=host[0])
 
 indexs = indexf
 
@@ -31,7 +51,27 @@ for index in range(len(P)):
 
         indexf = index
 
-py.plot(P[indexs:indexf+1],HP[indexs:indexf+1],marker[1],label=host[1])
+for index in range(indexs,indexf+1):
+
+    if HP[index] == 1.:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[1],mfc='k',fmt='',ls='None',label=host[1])
+
+    elif HP[index] < 1. and HP[index] >= .5:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[1],mfc='b',fmt='',ls='None',label=host[1])
+
+    elif HP[index] < .5 and HP[index] >= .3:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[1],mfc='g',fmt='',ls='None',label=host[1])
+
+    elif HP[index] < .3 and HP[index] >= .1:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[1],mfc='r',fmt='',ls='None',label=host[1])
+
+    elif HP[index] < .1 :
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[1],mfc='y',fmt='',ls='None',label=host[1])
 
 indexs = indexf
 
@@ -41,7 +81,27 @@ for index in range(len(P)):
 
         indexf = index
 
-py.plot(P[indexs:indexf+1],HP[indexs:indexf+1],marker[2],label=host[2])
+for index in range(indexs,indexf+1):
+
+    if HP[index] == 1.:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[2],mfc='k',fmt='',ls='None',label=host[2])
+
+    elif HP[index] < 1. and HP[index] >= .5:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[2],mfc='b',fmt='',ls='None',label=host[2])
+
+    elif HP[index] < .5 and HP[index] >= .3:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[2],mfc='g',fmt='',ls='None',label=host[2])
+
+    elif HP[index] < .3 and HP[index] >= .1:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[2],mfc='r',fmt='',ls='None',label=host[2])
+
+    elif HP[index] < .1 :
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[2],mfc='y',fmt='',ls='None',label=host[2])
 
 indexs = indexf
 
@@ -51,7 +111,27 @@ for index in range(len(P)):
 
         indexf = index
 
-py.plot(P[indexs:indexf+1],HP[indexs:indexf+1],marker[3],label=host[3])
+for index in range(indexs,indexf+1):
+
+    if HP[index] == 1.:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[3],mfc='k',fmt='',ls='None',label=host[3])
+
+    elif HP[index] < 1. and HP[index] >= .5:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[3],mfc='b',fmt='',ls='None',label=host[3])
+
+    elif HP[index] < .5 and HP[index] >= .3:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[3],mfc='g',fmt='',ls='None',label=host[3])
+
+    elif HP[index] < .3 and HP[index] >= .1:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[3],mfc='r',fmt='',ls='None',label=host[3])
+
+    elif HP[index] < .1 :
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[3],mfc='y',fmt='',ls='None',label=host[3])
 
 indexs = indexf
 
@@ -61,7 +141,27 @@ for index in range(len(P)):
 
         indexf = index
 
-py.plot(P[indexs:indexf+1],HP[indexs:indexf+1],marker[4],label=host[4])
+for index in range(indexs,indexf+1):
+
+    if HP[index] == 1.:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[4],mfc='k',fmt='',ls='None',label=host[4])
+
+    elif HP[index] < 1. and HP[index] >= .5:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[4],mfc='b',fmt='',ls='None',label=host[4])
+
+    elif HP[index] < .5 and HP[index] >= .3:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[4],mfc='g',fmt='',ls='None',label=host[4])
+
+    elif HP[index] < .3 and HP[index] >= .1:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[4],mfc='r',fmt='',ls='None',label=host[4])
+
+    elif HP[index] < .1 :
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[4],mfc='y',fmt='',ls='None',label=host[4])
 
 indexs = indexf
 
@@ -71,7 +171,27 @@ for index in range(len(P)):
 
         indexf = index
 
-py.plot(P[indexs:indexf+1],HP[indexs:indexf+1],marker[5],label=host[5])
+for index in range(indexs,indexf+1):
+
+    if HP[index] == 1.:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[5],mfc='k',fmt='',ls='None',label=host[5])
+
+    elif HP[index] < 1. and HP[index] >= .5:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[5],mfc='b',fmt='',ls='None',label=host[5])
+
+    elif HP[index] < .5 and HP[index] >= .3:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[5],mfc='g',fmt='',ls='None',label=host[5])
+
+    elif HP[index] < .3 and HP[index] >= .1:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[5],mfc='r',fmt='',ls='None',label=host[5])
+
+    elif HP[index] < .1 :
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[5],mfc='y',fmt='',ls='None',label=host[5])
 
 indexs = indexf
 
@@ -81,7 +201,27 @@ for index in range(len(P)):
 
         indexf = index
 
-py.plot(P[indexs:indexf+1],HP[indexs:indexf+1],marker[6],label=host[6])
+for index in range(indexs,indexf+1):
+
+    if HP[index] == 1.:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[6],mfc='k',fmt='',ls='None',label=host[6])
+
+    elif HP[index] < 1. and HP[index] >= .5:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[6],mfc='b',fmt='',ls='None',label=host[6])
+
+    elif HP[index] < .5 and HP[index] >= .3:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[6],mfc='g',fmt='',ls='None',label=host[6])
+
+    elif HP[index] < .3 and HP[index] >= .1:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[6],mfc='r',fmt='',ls='None',label=host[6])
+
+    elif HP[index] < .1 :
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[6],mfc='y',fmt='',ls='None',label=host[6])
 
 indexs = indexf
 
@@ -91,7 +231,27 @@ for index in range(len(P)):
 
         indexf = index
 
-py.plot(P[indexs:indexf+1],HP[indexs:indexf+1],marker[7],label=host[7])
+for index in range(indexs,indexf+1):
+
+    if HP[index] == 1.:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[7],mfc='k',fmt='',ls='None',label=host[7])
+
+    elif HP[index] < 1. and HP[index] >= .5:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[7],mfc='b',fmt='',ls='None',label=host[7])
+
+    elif HP[index] < .5 and HP[index] >= .3:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[7],mfc='g',fmt='',ls='None',label=host[7])
+
+    elif HP[index] < .3 and HP[index] >= .1:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[7],mfc='r',fmt='',ls='None',label=host[7])
+
+    elif HP[index] < .1 :
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[7],mfc='y',fmt='',ls='None',label=host[7])
 
 indexs = indexf
 
@@ -101,19 +261,39 @@ for index in range(len(P)):
 
         indexf = index
 
-py.plot(P[indexs:indexf+1],HP[indexs:indexf+1],marker[8],label=host[8])
+for index in range(indexs,indexf+1):
+
+    if HP[index] == 1.:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[8],mfc='k',fmt='',ls='None',label=host[8])
+
+    elif HP[index] < 1. and HP[index] >= .5:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[8],mfc='b',fmt='',ls='None',label=host[8])
+
+    elif HP[index] < .5 and HP[index] >= .3:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[8],mfc='g',fmt='',ls='None',label=host[8])
+
+    elif HP[index] < .3 and HP[index] >= .1:
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[8],mfc='r',fmt='',ls='None',label=host[8])
+
+    elif HP[index] < .1 :
+
+        py.errorbar(P[index],re[index],yerr=er[index],xerr=None,marker[8],mfc='y',fmt='',ls='None',label=host[8])
 
 py.xscale('log')
 
-py.yscale('log')
+py.yscale('linear')
 
-py.ylim(5.e-3,1.e1)
+#py.ylim(5.e-3,1.e1)
 
 py.xlim(1.e0,5.e2)
 
 py.xlabel('Period [days]')
 
-py.ylabel(r'$\alpha_{eff}$')
+py.ylabel('W [mag]')
 
 py.legend(loc=0,numpoints=1,ncol=4)
 
