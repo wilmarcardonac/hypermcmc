00f60b9d23481e0cd55bd10404de0e7dd1ff9e14
Wilmar Alberto Cardona Castro <wilmar.cardona@unige.ch>
2016-01-19 18:05:17 +0100

    BUG WRITING HPS FIXED

--- mcmc.f90
+++ mcmc.f90
@@ -5025,7 +5025,7 @@ Program mcmc
                                    
                                    If (PeriodR11(m) .lt. cepheid_Period_limit) then
 
-                                      If ( chi2R11_W(bestfit(n),bestfit(9),bestfit(10),bestfit(11),bestfit(13),prior_sigma_int,m)&
+                                      If ( chi2R11_W(bestfit(n),bestfit(9),bestfit(10),bestfit(11),bestfit(13),10**(bestfit(15)),m)&
                                         .le. 1.d0 ) then
 
                                          write(UNIT_HP_FILE,*) PeriodR11(m), observed_m_W(F160WR11(m),VIR11(m)) - &
@@ -5037,7 +5037,7 @@ Program mcmc
                                          write(UNIT_HP_FILE,*) PeriodR11(m), observed_m_W(F160WR11(m),VIR11(m)) - &
                                               P_L_relation_passband_W(bestfit(n),bestfit(9),bestfit(10),bestfit(11),bestfit(13),&
                                               OHR11(m),PeriodR11(m)), eF160WR11(m), 1.d0/chi2R11_W(bestfit(n),bestfit(9),&
-                                              bestfit(10),bestfit(11),bestfit(13),prior_sigma_int,m), Field(m)
+                                              bestfit(10),bestfit(11),bestfit(13),10**(bestfit(15)),m), Field(m)
 
                                       End If
 
